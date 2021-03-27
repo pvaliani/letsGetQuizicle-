918376f16ca00dd9465aba5dd93a0c01cbb474e1
Pedram Valiani <pedram@Pedrams-MacBook-Pro.local>
2021-03-27 15:15:36 +0000

    fetch call working

--- server/src/main/java/com/example/server/api/QuizAPI.java
+++ server/src/main/java/com/example/server/api/QuizAPI.java
@@ -1,6 +1,45 @@
 package com.example.server.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+// The code for the fetch is set up in here
+
+//Use the component annotation to allow the fetch
+
+@Component
 public class QuizAPI {
 
+//   performs a fetch for 50 questions
+
+    public JsonNode getQuestions() {
+        String url = "https://opentdb.com/api.php?amount=50&type=multiple";
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        ObjectMapper mapper = new ObjectMapper();
+
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+//      Represents the node we are dealing with - initialised to null
+        JsonNode root = null;
+
+//      Exception handling from the fetch
+
+        try {
+            root = mapper.readTree(response.getBody());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        JsonNode node = root.get(1);
+        JsonNode questionsNode = root.get("results");
+        System.out.println(questionsNode);
+        return questionsNode;
+    }
 
 }
--- server/src/test/java/com/example/server/ServerApplicationTests.java
+++ server/src/test/java/com/example/server/ServerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.server;
 
+import com.example.server.api.QuizAPI;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServerApplicationTests {
 
+	@Autowired
+	QuizAPI quizAPI;
+
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canCallAPI(){
+		quizAPI.getQuestions();
+
 	}
 
 }
